0616a971ac555d718182af0c1f2e80a90f7fb125
Marcos Valério <marcosvaleriox12@hotmail.com>
2019-06-16 20:53:25 -0300

    Remove retorno de Departamento.addColaborador

--- src/main/java/br/com/ap220191/ec04_controle_projetos/model/Departamento.java
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Departamento.java
@@ -15,7 +15,7 @@ public class Departamento
 	private ArrayList<Colaborador> colaboradores;
 	private ArrayList<Projeto> projetos;
 
-	public boolean addColaborador(Colaborador colaborador)
+	public void addColaborador(Colaborador colaborador)
 	{
             colaboradores.add(colaborador);
 	}
