1af1f484ebcceaa9a73b0773d9e4838f785ed410
expert975 <rodrigocomputadores125@hotmail.com>
2019-06-17 10:26:22 -0300

    Uniformiza estilo de código
    - Move chave do final da linha para o início da próxima.
    - Move '+' em concatenações para o início da linha.
    - Remove caminhos absolutos para 'java.lang'.

--- src/main/java/br/com/ap220191/ec04_controle_projetos/model/Endereco.java
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Endereco.java
@@ -1,6 +1,7 @@
 package br.com.ap220191.ec04_controle_projetos.model;
 
-public class Endereco {
+public class Endereco
+{
 	private String tipo;
 	private String nome;
 	private int numero;
@@ -10,81 +11,98 @@ public class Endereco {
 	private String cidade;
 	private String estado;
 
-	public String getTipo() {
+	public String getTipo()
+	{
 		return tipo;
 	}
 
-	public void setTipo(String tipo) {
+	public void setTipo(String tipo)
+	{
 		this.tipo = tipo;
 	}
 
-	public String getNome() {
+	public String getNome()
+	{
 		return nome;
 	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome)
+	{
 		this.nome = nome;
 	}
 
-	public int getNumero() {
+	public int getNumero()
+	{
 		return numero;
 	}
 
-	public void setNumero(int numero) {
+	public void setNumero(int numero)
+	{
 		this.numero = numero;
 	}
 
-	public String getComplemento() {
+	public String getComplemento()
+	{
 		return complemento;
 	}
 
-	public void setComplemento(String complemento) {
+	public void setComplemento(String complemento)
+	{
 		this.complemento = complemento;
 	}
 
-	public String getBairro() {
+	public String getBairro()
+	{
 		return bairro;
 	}
 
-	public void setBairro(String bairro) {
+	public void setBairro(String bairro)
+	{
 		this.bairro = bairro;
 	}
 
-	public String getCep() {
+	public String getCep()
+	{
 		return cep;
 	}
 
-	public void setCep(String cep) {
+	public void setCep(String cep)
+	{
 		this.cep = cep;
 	}
 
-	public String getCidade() {
+	public String getCidade()
+	{
 		return cidade;
 	}
 
-	public void setCidade(String cidade) {
+	public void setCidade(String cidade)
+	{
 		this.cidade = cidade;
 	}
 
-	public String getEstado() {
+	public String getEstado()
+	{
 		return estado;
 	}
 
-	public void setEstado(String estado) {
+	public void setEstado(String estado)
+	{
 		this.estado = estado;
 	}
 
-	@java.lang.Override
-		public java.lang.String toString() {
-		return "Endereco{" +
-			"tipo='" + tipo + '\'' +
-			", nome='" + nome + '\'' +
-			", numero=" + numero +
-			", complemento='" + complemento + '\'' +
-			", bairro='" + bairro + '\'' +
-			", cep='" + cep + '\'' +
-			", cidade='" + cidade + '\'' +
-			", estado='" + estado + '\'' +
-			'}';
+	@Override
+	public String toString()
+	{
+		return "Endereco{"
+			+ "tipo='" + tipo + "'"
+			+ ", nome='" + nome + "'"
+			+ ", numero='" + numero "'"
+			+ ", complemento='" + complemento + "'"
+			+ ", bairro='" + bairro + "'"
+			+ ", cep='" + cep + "'"
+			+ ", cidade='" + cidade + "'"
+			+ ", estado='" + estado + "'"
+			+ '}';
 	}
 }
