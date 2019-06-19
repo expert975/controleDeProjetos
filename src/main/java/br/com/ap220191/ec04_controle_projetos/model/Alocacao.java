d37364b50b5bd91ecd9cab71121f063ad0f0ad9c
Gabriel <gabr.fern99@gmail.com>
2019-06-19 12:58:02 -0300

    Implementa classe Alocacao e remove TODO

--- src/main/java/br/com/ap220191/ec04_controle_projetos/model/Alocacao.java
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Alocacao.java
@@ -3,9 +3,10 @@ package br.com.ap220191.ec04_controle_projetos.model;
 public class Alocacao
 {
 
-//TODO: Classe incompleta, por favor, complementar a classe
-
 	private int cargaHoraria;
+	private EstadoAlocacao estado;
+	private Colaborador colaborador;
+	private Projeto projeto;
 
 	public int getCargaHoraria()
 	{
@@ -15,5 +16,46 @@ public class Alocacao
 	public void setCargaHoraria(int cargaHoraria)
 	{
 		this.cargaHoraria = cargaHoraria;
+	}
+
+	public EstadoAlocacao getEstado()
+	{
+		return estado;
+	}
+
+	public void setEstado(EstadoAlocacao estado)
+	{
+		this.estado = estado;
+	}
+
+	public Colaborador getColaborador()
+	{
+		return colaborador;
+	}
+
+	public void setColaborador(Colaborador colaborador)
+	{
+		this.colaborador = colaborador;
+	}
+
+	public Projeto getProjeto()
+	{
+		return projeto;
+	}
+
+	public void setProjeto()
+	{
+		this.projeto = projeto;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Alocacao{"
+			+ "cargaHoraria=" + cargaHoraria
+			+ ", estado='" + estado + "'"
+			+ ", colaborador='" + colaborador + "'"
+			+ ", projeto='" + projeto + "'"
+			+ '}';
 	}
 }
