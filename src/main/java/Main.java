16c2e7c3c2e024466b578acc9b48dc1f7d9028a9
expert975 <rodrigocomputadores125@hotmail.com>
2019-07-03 18:25:43 -0300

    Adiciona exemplo de uso de ApiCorreios em Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
 import br.com.ap220191.ec04_controle_projetos.model.*;
 
+import java.lang.Thread;
+import java.lang.InterruptedException;
+
 public class Main
 {
 	public static void main(String args[])
 	{
 		System.out.println("Hello world");
+		ApiCorreios corr = ApiCorreios.getInstance();
+		try
+		{
+			System.out.println(corr.buscaEndereco("74474101"));
+			System.out.println(corr.buscaEndereco("74474201"));
+			System.out.println(corr.buscaEndereco("74494201"));
+		}
+		catch(CorreiosException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
