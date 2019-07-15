505b43ce5560858d4e18032220f856a33a2eea7a
Marcos Valério <marcosvaleriox12@hotmail.com>
2019-07-15 10:11:58 -0300

    implementa as mudanças no main

--- src/main/java/br/com/ap220191/ec04_controle_projetos/model/Main.java
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+        // TODOS atributos individuais tem que ser instanciados primeiro, mesmo que não sejam inicializados;
+        //OBS: Adicionar departamentos dá erro!
         ConversorData conversor = new ConversorData();
         Date dateInicio = conversor.converterData("30/07/2019");
         Date dateConclusao = conversor.converterData("30/08/2020");
@@ -11,16 +13,32 @@ public class Main {
         Date nascimento = conversor.converterData("22/11/1965");
         
         Telefone telefoneTI = new Telefone("12", "Embratel", Mobilidade.FIXO);
+        Telefone telefoneGerenteTI = new Telefone("13", "Tim", Mobilidade.MOVEL);
         
-        //Gerente gerenteTI = new Gerente();
-        //OBS: Adicionar um atributo por vez em cada construtor. Quando adicionei tudo de uma vez deu vários erros e não consegui resolver.
+        ArrayList<Departamento> departamentosTI = new ArrayList<>();
+        ArrayList<Projeto> projetosTI = new ArrayList<>();
+        ArrayList<Telefone> telefonesGerenteTI = new ArrayList<>();
+        ArrayList<Colaborador> colaboradoresTI = new ArrayList<>();
+                
+        Gerente gerenteTI = new Gerente("Arthur", "XXXXXXXXX", Sexo.MASCULINO, 
+                                        nascimento, 4000l, "email", null, 
+                                        telefonesGerenteTI, departamentosTI);
         
-        Departamento setorTecnologia = new Departamento("Tech", "TI", 4000000l, "setortech@hotmail.com",
-                                                        telefoneTI, null, null, null);
+        Colaborador colaboradorTI = new Colaborador("Jose", "CPF", Sexo.MASCULINO, 
+                                            nascimento, 4000l, "email", null, 
+                                            telefonesGerenteTI, departamentosTI);
+        
+        
+        Departamento setorTecnologia = new Departamento("Tech", "TI", 4000000l, 
+                                    "setortech@hotmail.com",telefoneTI, gerenteTI, 
+                                    colaboradoresTI, projetosTI);            
+        
+        Projeto projeto1 = new Projeto("Automacao", dateInicio, dateConclusao, 4000l, "Automatizar", setorTecnologia);
+        Projeto projeto2 = new Projeto("Desvio de Verba", dateInicio, dateConclusao, 5000000l, "Balburdia", setorTecnologia);        
+        projeto1.mudarSituacao(Situacao.ATRASADO, mudanca, colaboradorTI, null);
+        setorTecnologia.addProjeto(projeto1);
+        setorTecnologia.addProjeto(projeto2); //Não adiciona o projeto2 pq excede a verba
         System.out.println(setorTecnologia.toString());
-        
-        
-        //Projeto projeto1 = new Projeto("Automacao", dateInicio, dateConclusao, 4000l, "Automatizar", setorTecnologia);
         //System.out.println(projeto1.toString());
     }
 }
